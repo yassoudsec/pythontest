4bae7317eef59a54b87a77c2b12d35075a8f3ef8
Yassou <yassoukoin@gmail.com>
2019-09-15 18:29:47 +0000

    Add increment line

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "La somme de a et b est <b>" + str(sum_val(4,3)) + "</b>."
+    return "l'increment de 4 est <b>" + str(increment(5)) + "</b>."
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
